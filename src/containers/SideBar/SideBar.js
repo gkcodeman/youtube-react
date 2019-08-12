36acc23133e7426a6670db1bf590e4e23bfb8232
Jonathan Carroll <webdeveloper@jonathanlcarroll.com>
2019-08-12 12:53:20 -0400

    added dividers in between specific sidebar items using semantic ui Divider component

--- src/containers/SideBar/SideBar.js
+++ src/containers/SideBar/SideBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {SideBarItem} from './SideBarItem/SideBarItem';
-import {Menu} from 'semantic-ui-react';
+import {Menu, Divider} from 'semantic-ui-react';
 import './SideBar.scss';
 
 export class SideBar extends React.Component {
@@ -10,13 +10,17 @@ export class SideBar extends React.Component {
                <SideBarItem highlight={true} label='Home' icon='home' />
                <SideBarItem label='Trending' icon='fire' />
                <SideBarItem label='Followers' icon='spy' />
+               <Divider/>
                <SideBarItem label='History' icon='history' />
                <SideBarItem label='Watch later' icon='clock' />
                <SideBarItem label='Liked videos' icon='thumbs up' />
+               <Divider/>
                <SideBarItem label='Movies and Shows' icon='film' />
+               <Divider/>
                <SideBarItem label='Report history' icon='flag' />
                <SideBarItem label='Help' icon='help circle' />
                <SideBarItem label='Send feedback' icon='comment' />
+               <Divider/>
             </Menu>
         );
     }
