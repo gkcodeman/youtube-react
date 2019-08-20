4519cecb061b3df253d9d4e22b0858414e9fe08f
Jonathan Carroll <webdeveloper@jonathanlcarroll.com>
2019-08-20 10:06:35 -0400

    Created VideoGrid component to hold all VideoPreview components in a row for each section of at max 6 in each section topic. Updated Home Component, Home styling, set media queries for displaying videos responsively.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import HeaderNav from './containers/HeaderNav/HeaderNav';
-import {SideBar} from './containers/SideBar/SideBar';
+//import {SideBar} from './containers/SideBar/SideBar';
 import {Home} from './containers/Home/Home';
 import './containers/Home/Home.scss';
 
@@ -9,7 +9,6 @@ class App extends Component {
     return (
       <React.Fragment>
         <HeaderNav />
-        <SideBar />
         <Home />
       </React.Fragment>
     );
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import {VideoPreview} from '../../containers/VideoPreview/VideoPreview';
 import './Home.scss';
+import {VideoGrid} from '../../components/VideoGrid/VideoGrid';
+import {SideBar} from '../SideBar/SideBar';
 
 export class Home extends React.Component {
     render() {
         return (
-            <div className='home'>
-                <VideoPreview />
-            </div>
+            <React.Fragment>
+                <SideBar/>
+                <div className='home'>
+                    <div className='responsive-video-grid-container'>
+                        <VideoGrid title='Trending' />
+                        <VideoGrid title='Autos & Vehicles' hideDivider={true} />
+                    </div>
+                </div>
+            </React.Fragment>
+            
         )
     }
 }
